9d41783272955dfeb76d16ff0b52112ca7e899ac
thiago <thiago.piccioni@dhani.com>
2021-04-09 22:48:29 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='OCR Fast Api',
+    version='1.0.0',
+    description='API able at to convert an image into a text',
+    author='Thiago Piccioni',
+    author_email='thiagopiccioni@gmail.com',
+
+    classifiers=[
+        'Development Status :: 2 - Prototype/Not Production Ready',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+)
